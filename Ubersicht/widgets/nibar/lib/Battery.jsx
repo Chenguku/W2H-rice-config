7e6c62db418383b8384d2214772f59bc67c3c963
Chenguku <peasantrule@gmail.com>
2024-07-09 12:56:30 -0400

    Changed color on low battery This was done by setting style to a new object in nibar/lib/Battery.jsx This seems inefficient but I encountered a readonly error when trying to change style.color directly which I was unable to fix.

--- Ubersicht/widgets/nibar/lib/Battery.jsx
+++ Ubersicht/widgets/nibar/lib/Battery.jsx
@@ -1,20 +1,17 @@
 import styles from "./styles.jsx";
-const style = {
+let style = {
   color: styles.colors.extra2,
 };
 
 const render = ({ output }) => {
   let charging = output.charging;
   let percentage = output.percentage;
+  if(percentage <= 10 && !charging){
+    style = {color: styles.colors.red,}
+  };
   return (
     <div>
-      <div
-        style={
-          percentage < 10 || charging == false
-            ? { color: styles.colors.red }
-            : null
-        }
-      >
+      <div style={style}>
         <span style={style}>
           {charging ? "⚡ " : " "}
           {percentage}%
